b8c8f8e849155c909135d86bdb9bcdd58743b1c0
Kate5823 <70522517+Kate5823@users.noreply.github.com>
2022-04-19 12:02:36 +0300

    1

--- 1.js
+++ 1.js
@@ -1,8 +1,4 @@
 // Use this sample to create your own voice commands
 intent('hello world', p => {
-    p.play('(hedddddddddddddddddddllo|hi there)');
-});
-
-intent('sun', p => {
-    p.play('(moon)');
+    p.play('(hello|hi there)');
 });
